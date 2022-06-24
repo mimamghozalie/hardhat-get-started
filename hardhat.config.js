a38495414851fdefd4f6a39c7172a88a4406223d
Mochamad Imam Ghozalie <mimamghozalie@gmail.com>
2022-06-24 22:57:39 +0700

    add network & deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,11 @@
+// Secret
+const {
+  mnemonic,
+  privateKey,
+  infuraProjectId,
+  etherscanApiKey,
+} = require("./secrets.json");
+
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -24,4 +32,22 @@ module.exports = {
       runs: 1000,
     },
   },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    hardhat: {},
+    bsctestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      accounts: [privateKey],
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 1000000000,
+      accounts: [privateKey],
+    },
+  },
+  defaultNetwork: "localhost",
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  // We get the contract to deploy
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+  await greeter.deployed();
+
+  console.log("Contract address:", greeter.address);
+  console.log("Gas Fee:", greeter.deployTransaction.gasLimit.toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
